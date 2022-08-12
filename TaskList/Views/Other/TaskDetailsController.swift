dc66ad122767603d0ea201c4a069007b45e673ae
David Riegel <40246197+bumothekid@users.noreply.github.com>
2022-08-12 17:30:07 +0200

    Completion and Edit Button added

--- TaskList/Views/Other/TaskDetailsController.swift
+++ TaskList/Views/Other/TaskDetailsController.swift
@@ -12,7 +12,7 @@ class TaskDetailsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if task == nil {
+        if task == nil || index == nil {
             back()
         }
         
@@ -20,6 +20,7 @@ class TaskDetailsController: UIViewController {
     }
     
     var task: Dictionary<String, Any>!
+    var index: Int!
     
     // MARK: -- Views
     
@@ -74,8 +75,50 @@ class TaskDetailsController: UIViewController {
         label.font = .systemFont(ofSize: 16, weight: .semibold)
         return label
     }()
+
+    lazy var completeTaskButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .secondaryColor
+        button.addTarget(self, action: #selector(completeTask), for: .touchUpInside)
+        button.layer.cornerRadius = 35
+        button.tintColor = .systemGreen
+        button.setImage(UIImage(systemName: "checkmark", withConfiguration: UIImage.SymbolConfiguration(font: UIFont.systemFont(ofSize: 18, weight: .bold))), for: .normal)
+        return button
+    }()
+
+    lazy var editTaskButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .secondaryColor
+        button.addTarget(self, action: #selector(back), for: .touchUpInside)
+        button.layer.cornerRadius = 35
+        button.tintColor = .systemBlue
+        button.setImage(UIImage(systemName: "square.and.pencil", withConfiguration: UIImage.SymbolConfiguration(font: UIFont.systemFont(ofSize: 18, weight: .bold))), for: .normal)
+        return button
+    }()
+
     
     // MARK: -- Objective C Functions
+    
+    @objc
+    func completeTask() {
+        let alert = UIAlertController(title: "", message: "Are you sure you want to complete this Task?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { [self] _ in
+            var tasks = UserDefaults.standard.array(forKey: "tasks") as? [Dictionary<String, Any>]
+            
+            if tasks == nil {
+                tasks = [Dictionary<String, Any>]()
+            }
+            
+            tasks![index]["done"] = true
+            
+            UserDefaults.standard.set(tasks, forKey: "tasks")
+            
+            back()
+        }))
+        
+        present(alert, animated: true)
+    }
     
     @objc
     func back() {
@@ -116,5 +159,11 @@ class TaskDetailsController: UIViewController {
         dueBackgroundView.addSubview(dueToLabel)
         dueToLabel.anchor(left: dueBackgroundView.leftAnchor, right: dueDatePicker.rightAnchor, paddingLeft: 8, paddingRight: -8)
         dueToLabel.centerYAnchor.constraint(equalTo: dueBackgroundView.centerYAnchor).isActive = true
+        
+        view.addSubview(completeTaskButton)
+        completeTaskButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: -20, paddingRight: -20, width: 70, height: 70)
+        
+        view.addSubview(editTaskButton)
+        editTaskButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingLeft: 20, paddingBottom: -20, width: 70, height: 70)
     }
 }
