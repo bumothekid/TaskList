9b7dabd203d26f8c05985293eaba744fce9840cd
David Riegel <40246197+bumothekid@users.noreply.github.com>
2022-08-13 10:57:02 +0200

    Removed old code

--- TaskList/Resources/AppDelegate.swift
+++ TaskList/Resources/AppDelegate.swift
@@ -14,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-//        if UserDefaults.standard.array(forKey: "tasks") == nil {
-//            let firstTask: [Dictionary<String, Any>] = [["title": "Your first task", "description": "Create your first task in the top right corner", "done": false, "date": "red"], ["title": "Your second first yes yeah task", "description": "How are you really today please tell me please yes mhm yeah same", "done": false, "color": "gray"]]
-//            UserDefaults.standard.set(firstTask, forKey: "tasks")
-//        }
 
         let window = UIWindow(frame: UIScreen.main.bounds)
         
